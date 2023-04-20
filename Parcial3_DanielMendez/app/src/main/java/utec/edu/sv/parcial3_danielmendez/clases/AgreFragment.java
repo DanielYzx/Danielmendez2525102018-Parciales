1821deb30635c7587d9c456d88a1198c8bb3e6cd
DanielMéndez <2525102018@mail.utec.edu.sv>
2023-04-19 21:54:55 -0600

    primer

--- Parcial3_DanielMendez/app/src/main/java/utec/edu/sv/parcial3_danielmendez/clases/AgreFragment.java
+++ Parcial3_DanielMendez/app/src/main/java/utec/edu/sv/parcial3_danielmendez/clases/AgreFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +14,15 @@ import androidx.fragment.app.Fragment;
 import utec.edu.sv.parcial3_danielmendez.R;
 
 public class AgreFragment extends Fragment {
+    EditText txtNombre,txtApellido,txtTelefono,txtCorreo;
+    Button btnAgregar;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
       return inflater.inflate(R.layout.fragmento_agregar,container,false);
+
+
 
     }
 
